9c537fddf5ff419bb056e1b936739dcbe6ceaea2
Maicon <maicon.silva@sinqia.com.br>
2022-03-29 18:55:40 -0300

    faltou virgula

--- src/main/kotlin/br/com/maicon/forum/model/Topico.kt
+++ src/main/kotlin/br/com/maicon/forum/model/Topico.kt
@@ -9,7 +9,7 @@ data class Topico(
     val dataCriacao: LocalDateTime = LocalDateTime.now(),
     val curso: Curso,
     val autor: Usuario,
-    val status: StatusTopico = StatusTopico.NAO_RESPONDIDO
+    val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
     val respostas: List<Resposta> = ArrayList()
 
 )
