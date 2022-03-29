8032208e3ae6946fc3401d20be5231365773ad64
Maicon <maicon.silva@sinqia.com.br>
2022-03-29 17:58:22 -0300

    Add project files.

---
+++ src/main/kotlin/br/com/maicon/forum/model/Topico.kt
@@ -0,0 +1,14 @@
+package br.com.maicon.forum.model
+
+import java.time.LocalDateTime
+
+data class Topico(
+    val id: Long? = null,
+    val titulo: String,
+    val mensagem: String,
+    val dataCriacao: LocalDateTime = LocalDateTime.now(),
+    val curso: Curso,
+    val 
+)
+
+
