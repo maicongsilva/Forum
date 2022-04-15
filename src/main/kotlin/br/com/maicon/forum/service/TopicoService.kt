824bbc2a4eb62674e12acf4b313020f0602955e9
Maicon <maicon.silva@sinqia.com.br>
2022-04-15 19:51:40 -0300

    Adequando api a boas praticas

--- src/main/kotlin/br/com/maicon/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/maicon/forum/service/TopicoService.kt
@@ -40,17 +40,18 @@ class TopicoService(
         return topico
     }
 
-    fun cadastrar(form: NovoTopicoForm){
+    fun cadastrar(form: NovoTopicoForm): TopicoView{
        val topico = topicoFormMapper.map(form)
         topico.id = topicos.size.toLong() + 1
         topicos =  topicos.plus(topico)
+        return topicoViewMapper.map(topico)
     }
 
-    fun atualizar(form: AtualizacaoTopicoForm) {
+    fun atualizar(form: AtualizacaoTopicoForm): TopicoView{
         val topico = topicos.stream().filter({
                 t -> t.id == form.id
         }).findFirst().get()
-        topicos = topicos.minus(topico).plus(Topico(
+        val topicoAtualizado = Topico(
             id = form.id,
             titulo = form.titulo,
             mensagem = form.mensagem,
@@ -59,8 +60,15 @@ class TopicoService(
             status = topico.status,
             dataCriacao = topico.dataCriacao,
             respostas = topico.respostas
-        ))
+        )
+        topicos = topicos.minus(topico).plus(topicoAtualizado)
+        return topicoViewMapper.map(topicoAtualizado)
     }
 
-
+    fun deletar(id: Long) {
+        val topico = topicos.stream().filter({
+                t -> t.id == id
+        }).findFirst().get()
+        topicos = topicos.minus(topico)
+    }
 }
